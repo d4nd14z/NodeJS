e2ccaf3ebaf462c5b3b87188c9eb37cf3eed53cf
d4nd14z <dandiazfo@gmail.com>
2021-04-10 08:07:03 -0500

    Creacion de Rutas y EndPoints (get,post,put,delete) en index.js (2021-04-10)

--- index.js
+++ index.js
@@ -11,21 +11,64 @@
 
 const express = require("express");
 const app = express();
+const host = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//Rutas normales
-app.get("/", (req, res) => {
-    res.send({ message: "Hola mundo !!!"});
+/**
+ * Ruta API EndPoint para obtener el listado de todos los productos almacenados en la B.D.
+ * @method: GET
+ * @author: @d4nd14z
+ * @since: 2021-04-10
+ */
+app.get("/demo/product", (req, res) => {
+    res.status(200).send({ products: [] });
 });
 
-//Rutas con parametros
-app.get("/:name", (req, res) => {
-    res.send({ message: `Hola, ${ req.params.name }`});
+/**
+ * Ruta API EndPoint para obtener el registro de un unico producto almacenado en la B.D.
+ * @method: GET
+ * @author: @d4nd14z
+ * @since: 2021-04-10
+ */
+app.get("/demo/product/:productId", (req, res) => {
+
 });
+
+/**
+ * Ruta API EndPoint para insertar (registrar)un producto dentro de la B.D.
+ * @method: POST
+ * @author: @d4nd14z
+ * @since: 2021-04-10
+ */
+app.post("/demo/product", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ message: 'El producto se ha recibido' });
+});
+
+/**
+ * Ruta API EndPoint para editar (actualizar) un producto almacenado de la B.D.
+ * @method: PUT
+ * @author: @d4nd14z
+ * @since: 2021-04-10
+ */
+app.put("/demo/product/:productId", (req, res) => {
+
+});
+
+/**
+ * Ruta API EndPoint para borrar (eliminar) un producto almacenado en la B.D.
+ * @method: DELETE
+ * @author: @d4nd14z
+ * @since: 2021-04-10
+ */
+app.delete("/demo/product/:productId", (req, res) => {
+
+});
+
 
 app.listen(3000, () => {
-    console.log(`D2(RS) Project - API RESTFull Running on http://localhost:${port} .......................... (OK)`);
+    console.log(`D2(RS) Project - API RESTFull Running on http://${host}:${port} .......................... (OK)`);
 });
