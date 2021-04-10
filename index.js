bcb7fe2764bb56f2948a80a68ecdb37fad07b581
d.diaz <d.diaz@tripode.co>
2021-04-10 15:54:01 -0500

    Implementacion de metodos CRUD - para /demo/products (2021-04-10)

--- index.js
+++ index.js
@@ -31,7 +31,29 @@ app.use(express.urlencoded({ extended: false }));
  * @since: 2021-04-10
  */
 app.get("/demo/product", (req, res) => {
-    res.status(200).send({ products: [] });
+    Product.find({}, (err, products) => {
+        if (!err){
+            if (!products){
+                return res.status(404).send({
+                    code: 404,
+                    data: null,
+                    message: `Not Found`
+                });
+            }
+            res.status(200).send({
+                code: 200,
+                data: products,
+                message: 'Operacion Exitosa'
+            });
+        }
+        else{
+            return res.status(400).send({
+                code: 400,
+                data: null,
+                message: `Bad Request`
+            });
+        }
+    });
 });
 
 /**
@@ -41,7 +63,30 @@ app.get("/demo/product", (req, res) => {
  * @since: 2021-04-10
  */
 app.get("/demo/product/:productId", (req, res) => {
-
+    let productId = req.params.productId;
+    Product.findById(productId, (err, stored) => {
+        if (!err){
+            if (!stored) { 
+                return res.status(404).send({
+                    code: 404,
+                    data: null,
+                    message: `Not Found`
+                }); 
+            }
+            res.status(200).send({
+                code: 200,
+                data: stored,
+                message: 'Operacion Exitosa'
+            });
+        }  
+        else{
+            return res.status(400).send({
+                code: 400,
+                data: null,
+                message: `Bad Request`
+            });
+        }
+    });
 });
 
 /**
@@ -57,7 +102,6 @@ app.post("/demo/product", (req, res) => {
     product.price = req.body.price;
     product.category = req.body.category;
     product.description = req.body.description;
-
     product.save((err, stored) => {
         if (!err){ 
             res.status(200).send({
@@ -70,7 +114,7 @@ app.post("/demo/product", (req, res) => {
             res.status(500).send({
                 code: 500,
                 data: null,
-                message: "No se ha podido almacenar el producto en la base de datos."
+                message: `No se ha podido almacenar el producto en la base de datos. ${err}`
             });
         }
     });
@@ -83,7 +127,33 @@ app.post("/demo/product", (req, res) => {
  * @since: 2021-04-10
  */
 app.put("/demo/product/:productId", (req, res) => {
-
+    let productId = req.params.productId;
+    let update = req.body;
+    Product.findByIdAndUpdate(productId, update, (err, updated) => {
+        if (!err){
+            if (!update){
+                return res.status(400).send({
+                    code: 400,
+                    data: null,
+                    message: `Bad Request`
+                });
+            }
+            else{
+                res.status(200).send({
+                    code: 200,
+                    data: updated,
+                    message: 'Operacion Exitosa'
+                });
+            }
+        }
+        else{
+            return res.status(500).send({
+                code: 500,
+                data: null,
+                message: `No se ha podido actualizar el producto en la base de datos. ${err}`
+            });
+        }
+    });
 });
 
 /**
@@ -93,7 +163,32 @@ app.put("/demo/product/:productId", (req, res) => {
  * @since: 2021-04-10
  */
 app.delete("/demo/product/:productId", (req, res) => {
-
+    let productId = req.params.productId;
+    Product.findById(productId, (err, product) => {
+        if (!err){
+            if (!product){
+                return res.status(404).send({
+                    code: 404,
+                    data: null,
+                    message: `Not Found`
+                });
+            }
+            product.remove(err => {
+                res.status(200).send({
+                    code: 200,
+                    data: null,
+                    message: 'Operacion Exitosa'
+                });
+            });
+        }  
+        else{
+            return res.status(500).send({
+                code: 500,
+                data: null,
+                message: `No se ha podido eliminar el producto en la base de datos. ${err}`
+            });
+        }
+    });
 });
 
 
