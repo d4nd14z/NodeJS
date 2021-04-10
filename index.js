cc46af2fa6d4af29434be25fbdf0ff1ee82af8af
d4nd14z <dandiazfo@gmail.com>
2021-04-10 06:24:39 -0500

    Version Inicial del Proyecto (2021-04-10)

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * D2(RS) Project 
+ * RESTFull API
+ * Love or Hate It
+ * Fucking Coronavirus (Covid-19) - Wash your hands
+ * @author: @d4nd14z
+ * @since: Apr 10 / 2021
+ */
+
+'use strict'
+
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("D2(RS) API RESTFull Running on http://localhost:3000 .......................... (OK)")
+});
